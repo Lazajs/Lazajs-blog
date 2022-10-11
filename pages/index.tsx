a79e8bdec2c2d2af12b54f70e0d9c33b822781d8
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2022-10-11 11:48:34 -0300

    Refactor: Eslint config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,29 @@
 import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
+import {getAllFilesMetadata} from '../lib/mdx'
+import Link from 'next/link'
+import styles from '../styles/Home.style'
 
-const Home: NextPage = () => {
+const Home: NextPage = ({posts} : any) => {
+  console.log(posts)
   return (
     <>
-    <div className={styles.container}>
-      <h1>Coming soon.</h1>
-      <p>Lazajs blog, information, contact. All about me.</p>
-    </div>
+    <h1>askdfhjlasdjfk</h1>
+      {posts.map((post: any) => { 
+        return (
+          <Link key={post.slug} href={post.slug}><a>POST</a></Link>
+        )
+      })}
+
+      <style jsx>{styles}</style>
     </>
   )
+}
+
+export async function getStaticProps () {
+  const posts = getAllFilesMetadata()
+  return {
+    props: {posts}
+  }
 }
 
 export default Home
---
+++ styles/Home.style.ts
@@ -0,0 +1,9 @@
+import css from "styled-jsx/css";
+
+// css.global
+
+export default css`
+  h1 {
+    color: red;
+  }
+`
