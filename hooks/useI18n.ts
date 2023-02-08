bce48db71bf03bef0e51f5fe40cc11c6766a6524
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2023-02-07 23:01:16 -0300

    fix: JSON types

--- hooks/useI18n.ts
+++ hooks/useI18n.ts
@@ -1,14 +1,19 @@
 import { useRouter } from 'next/router'
-import { FILTERS } from 'constants/default'
 import i18n from 'i18n/index.json'
 
-type Key = keyof typeof FILTERS
-type Name = keyof typeof i18n
+type JSONRaw = typeof i18n
+type Name = keyof JSONRaw
+type Language = keyof JSONRaw[Name]
+type Selection = JSONRaw[Name]
+type Property = Pick<Selection, Language>
+type PropertyValues = Property[Language]
 
 export default function useI18n (name: Name) {
   const { locale } = useRouter()
-  const wantedTextList = i18n[name]
-  const localeLanguage = locale as Key
+
+  const JSONFile: JSONRaw = i18n
+  const wantedTextList = JSONFile[name]
+  const localeLanguage = locale as Language
   const value = wantedTextList[localeLanguage]
-  return value as any
+  return value as PropertyValues
 }
