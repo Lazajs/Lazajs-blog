820e2012657989d571b6ae1b63c16c0d010640cc
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2023-02-08 17:15:38 -0300

    Fix: JSON types in hook

--- hooks/useI18n.ts
+++ hooks/useI18n.ts
@@ -4,16 +4,11 @@ import i18n from 'i18n/index.json'
 type JSONRaw = typeof i18n
 type Name = keyof JSONRaw
 type Language = keyof JSONRaw[Name]
-type Selection = JSONRaw[Name]
-type Property = Pick<Selection, Language>
-type PropertyValues = Property[Language]
 
-export default function useI18n (name: Name) {
+export default function useI18n<N extends Name> (name: N): JSONRaw[N][Language] {
   const { locale } = useRouter()
-
   const JSONFile: JSONRaw = i18n
   const wantedTextList = JSONFile[name]
-  const localeLanguage = locale as Language
-  const value = wantedTextList[localeLanguage]
-  return value as PropertyValues
+  const value = wantedTextList[locale as Language]
+  return value
 }
