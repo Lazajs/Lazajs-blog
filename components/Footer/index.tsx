40f11d35ce86952383fabc3aaa245af947830b48
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2023-08-04 01:59:40 -0300

    Revert "fix:footer"
    This reverts commit 6225ef5b799f2994c226207c5f527c9ff20fb325.

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -12,6 +12,7 @@ export default function Footer ({ data }: {data: PostData}) {
       <footer className="footer">
         {data?.traduction && <span><p className='translator'>{translator}: {data?.traduction} -</p><a href={`mailto:agustinagodoy04@gmail.com?Subject=${subject}`} target='_blank' rel="noreferrer">Contact mail</a></span>}
         <a className='author' href={'/lazaro'}>{`${author}: Lázaro Sánchez`}</a>
+        <p>MIT License</p>
         <a href="https://github.com/Lazajs/Lazajs-blog" target={'_blank'} rel="noreferrer"><Image style={{ filter: 'invert()' }} className='image' src={github} alt='source code' title='Source code' width={50} height={50}/></a>
       </footer>
 
