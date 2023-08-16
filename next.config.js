88180a910a14e17859e30197e942850403e4154f
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2023-08-16 03:58:10 -0300

    update: next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-const { NextFederationPlugin } = require('@module-federation/nextjs-mf')
 
 const nextConfig = {
   reactStrictMode: false,
@@ -8,25 +7,6 @@ const nextConfig = {
     locales: ['en-us', 'es-ar'],
     defaultLocale: 'es-ar',
     localeDetection: true
-  },
-  webpack: (config, options) => {
-    const { isServer } = options
-    config.plugins.push(
-      new NextFederationPlugin({
-        name: 'host',
-        filename: 'static/chunks/remoteEntry.js',
-        remotes: {
-          section_mf: process.env.DEVELOPMENT
-            ? `section_mf@http://localhost:3002/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`
-            : `section_mf@https://mf-lazajs.vercel.app/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`
-        },
-        extraOptions: {
-          // exposePages: true,
-          // automaticAsyncBoundary: true
-        }
-      })
-    )
-    return config
   }
 }
 
