d43c34573b370722478bf3c7d261645401803fb6
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2023-08-24 19:24:39 -0300

    add: experience

--- pages/lazaro/index.tsx
+++ pages/lazaro/index.tsx
@@ -4,6 +4,17 @@ import Image from 'next/image'
 import H from 'components/Higlight'
 import MyStack from 'components/MyStack'
 import AnimatedElements from 'components/AnimatedElements'
+import {AiFillGithub, AiFillLinkedin, AiOutlineMail} from 'react-icons/ai'
+
+const EXPERIENCE = [
+  {
+    name: 'No Country',
+    from: 'Abril 2023',
+    to: 'Ocasionalmente',
+    role: ['Front-end', 'Team Leader'],
+    description: 'A lo largo de tres proyectos desempeñé el rol de Front-end con equipos diferentes, desarrollando proyectos como "Awericana", "Managym" y "Wave". También estuve a cargo de dos equipos representando al cliente como Team Leader.'
+  }
+]
 
 export default function Lazaro () {
   const getAge = (birth: string) => { // YYYY-MM-DD
@@ -36,14 +47,24 @@ export default function Lazaro () {
           <span className='info'>
             <h1><H>L</H>áza<H>r</H>o S<H>ánc</H>hez</h1>
             <small>Desarrollador web, <H>{getAge('2004-04-26')}</H> años.</small>
+            <span className='network'>
+              <a href="https://www.linkedin.com/in/lazajs/" target='_blank'>
+                <AiFillLinkedin size={40} />
+              </a>
+              <a href="https://github.com/Lazajs" target='_blank'>
+                <AiFillGithub size={40} />
+              </a>
+              <a href="mailto:contact@lazajs.com" title='contact@lazajs.com'>
+                <AiOutlineMail size={40} />
+              </a>
+            </span>
           </span> 
-          <p></p>
         </header>
 
         <div className="description">
           <p>
-            Mi enfoque principal es la creación de experiencias de usuario <H>excepcionales</H>,
-             y me entusiasma la <H>optimización de rendimiento</H> y la implementación de sólidas <H>arquitecturas</H> web.
+            Mi enfoque principal es la creación de experiencias de usuario <H>excepcionales</H>.
+             Me entusiasma la <H>optimización de rendimiento</H> y la implementación de sólidas <H>arquitecturas</H> web.
           </p>
         </div>
 
@@ -51,9 +72,25 @@ export default function Lazaro () {
           <MyStack />
         </AnimatedElements>
 
-        {/* <article>
+        <section className='experience'>
           <h2>Experiencia</h2>
-        </article> */}
+            <AnimatedElements>
+              {
+                EXPERIENCE.map(experience => {
+                  return (
+                    <article key={experience.name} className='single'>
+                      <span className='data'>
+                        <h3><H>{experience.name}</H></h3>
+                        <small>{experience.from} - {experience.to}</small>
+                      </span>
+                      <p>{experience.description}</p>
+                    </article>
+                  )
+                })
+              }
+            </AnimatedElements>
+        </section>
+
       </section>
       <style jsx>{style}</style>
     </>
--- styles/Lazaro.style.ts
+++ styles/Lazaro.style.ts
@@ -30,15 +30,18 @@ export default css`
   .info {
     color: ${colors.text};
     height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: baseline;
+    justify-content: space-between;
   }
 
   .info h1 {
     font-size: 3rem;
-    margin-bottom: 10px;
   }
 
   h2 {
-    font-size: 2.8rem;
+    font-size: 3rem;
     color: ${colors.text};
   }
 
@@ -55,7 +58,69 @@ export default css`
     font-weight: 500;
   }
 
+  .network {
+    display: block;
+  }
+
+  .network a {
+    text-decoration: none;
+    color: ${colors.text};
+    
+    &:nth-child(2) {
+      margin-left: 10px;
+      margin-right: 10px;
+    }
+  }
+
+  .experience {
+    margin-top: 40px;
+    color: ${colors.text};
+    height: fit-content;
+    z-index: 20;
+    position: relative;
+
+    &::after {
+      content: '';
+      display: inline-block;
+      position: absolute;
+      left: 0;
+      top: 80px;
+      height: calc(100% - 50px);
+      width: 3px;
+      border-radius: 7px;
+      background-color: ${colors.react};
+    }
+  }
+
+  .experience .single {
+    margin: 20px;
+    margin-top: 40px;
+    margin-left: 40px;
+    font-size: 18px;
+  }
+
+  .experience h3 {
+    font-size: 2.4rem;
+    white-space: nowrap;
+  }
+
+  .single .data {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: baseline;
+    gap: 15px;
+    margin-bottom: 10px;
+  }
+
+  .data small {
+    font-size: 14px;
+    color: ${colors.subText};
+  }
+
   @media (min-width:768px) {
+    .info {
+      gap: 10px;
+    }
     .info h1 {
       font-size: 4rem;
     }
